b943bed0cf254f326f094d8b2fb75abd243b5f02
TANYA WISE <tanya.wise@gvs.ga.gov>
2017-06-28 18:20:52 -0400

    home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  def pets
+  def home
   end
 
   def supplies
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'welcome/supplies'
 
+get 'welcome/pets' => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
