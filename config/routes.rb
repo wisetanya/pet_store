a3e6d6058e023831f5e598dd3df1ba487d6d596c
TANYA WISE <tanya.wise@gvs.ga.gov>
2017-06-28 19:29:33 -0400

    id

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :pets
-  root 'welcome#pets', page: "home"
+  get 'welcome/pets'
+  
+  root 'welcome#pets'
 
   get 'welcome/supplies'
 
- get 'home' => 'welcome#pets'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
